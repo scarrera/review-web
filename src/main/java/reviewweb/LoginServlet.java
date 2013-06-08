a4feb8b24df1f99c5c638fe00922539dbcb97ad4
Santiago <santiagouc@gmail.com>
2013-06-08 15:35:21 -0300

    adding initial project

---
+++ src/main/java/reviewweb/LoginServlet.java
@@ -0,0 +1,21 @@
+package reviewweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Content-Type", "text/html");
+		response.getOutputStream().print("eeeh o no");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=ISO-8859-1" %>
+<html>
+<head>
+</head>
+<body>
+	<h1>UMFlix: Review website</h1>
+	<form action="login" method="post">
+		<label>Username (email): </label>
+		<input type="text" name="username">
+		<br>
+		<label>Password: </label>
+		<input type="password" name="password">
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
